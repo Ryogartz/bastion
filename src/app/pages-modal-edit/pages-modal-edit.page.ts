42886e17d979d484841090aecd08a79f3e0bad4c
Ramon Figuera <refm.130995@gmail.com>
2020-08-18 16:02:14 -0400

    Modal de edit

--- src/app/pages-modal-edit/pages-modal-edit.page.ts
+++ src/app/pages-modal-edit/pages-modal-edit.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pages-modal-edit',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pages-modal-edit.page.scss'],
 })
 export class PagesModalEditPage implements OnInit {
-
-  constructor() { }
+  @Input() firstName: string;
+  @Input() lastName: string;
+  @Input() middleInitial: string;
+  constructor(public modalCtrl: ModalController) { }
 
   ngOnInit() {
+  }
+  dismiss() {
+    // using the injected ModalController this page
+    // can "dismiss" itself and optionally pass back data
+    this.modalCtrl.dismiss({
+      'dismissed': true
+    });
   }
 
 }
--- src/app/pages/red/red.page.ts
+++ src/app/pages/red/red.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { PagesModalEditPage } from '../../pages-modal-edit/pages-modal-edit.page';
 
 @Component({
   selector: 'app-red',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RedPage implements OnInit {
 
-  constructor() { }
+  constructor(public modalController: ModalController) { }
 
   ngOnInit() {
+  }
+
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: PagesModalEditPage,
+      cssClass: 'my-custom-class',
+      swipeToClose: true,
+      presentingElement: await this.modalController.getTop(),
+      animated:true,
+      componentProps: {
+        'firstName': 'Douglas',
+        'lastName': 'Adams',
+        'middleInitial': 'N'
+      }
+    });
+    return await modal.present();
   }
 
 }
